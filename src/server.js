b3e6ef18174a59bee681939cec80ff9cd9aabd92
SUYEON KIM <yeonee230@gmail.com>
2023-03-27 00:09:41 +0900

    setup pug

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import {join} from "path";
 
 const PORT = 5005;
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+
+app.get("/", (req, res) => res.render("home"));
 const handleListening = () => console.log(`🔥 Server Listening! port : ${PORT}`);
 
 app.listen(PORT, handleListening);
